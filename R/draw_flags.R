208e7ca5feee5b7e42b4a5ce1f15386b13797b4a
Nicolas Casajus <nicolas.casajus@gmail.com>
2016-06-02 23:48:55 -0400

    first commit

--- R/draw_flags.R
+++ R/draw_flags.R
@@ -11,7 +11,7 @@ function(flags){
         for (j in 1 : 3){
             xmn[k] <- (i-1)+.025 ; xmx[k] <- i-.025
             ymn[k] <- (j-1)+.040 ; ymx[k] <- j-.040
-            x <- readPNG(system.file(paste('external/png/', tolower(flags[k]), '.png', sep = ), package = "geographr"))
+            x <- readPNG(system.file(paste('external/png/', tolower(flags[k]), '.png', sep = ''), package = "geographr"))
             rasterImage(x, xmn[k], ymn[k], xmx[k], ymx[k])
             rect(xmn[k], ymn[k], xmx[k], ymx[k], border = 'white', lwd = 3)
             k <- k + 1
--- R/map_flag.R
+++ R/map_flag.R
@@ -1,7 +1,7 @@
 map_flag <-
 function(iso){
 
-    x <- readPNG(system.file(paste('external/png/', tolower(iso), '.png', sep = ), package = "geographr"))
+    x <- readPNG(system.file(paste('external/png/', tolower(iso), '.png', sep = ''), package = "geographr"))
     par(mar = c(6, 2, 6, 2))
     plot(0, type = 'n', ann = FALSE, axes = FALSE)
     rasterImage(x, par()$usr[1], par()$usr[3], par()$usr[2], par()$usr[4])
